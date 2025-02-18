3a7fac1338759998d47f1bc2ddeaa750b78aca70
Ceazor <73511897+Ceazor@users.noreply.github.com>
2025-02-18 12:21:52 -0700

    added space

--- src/interfaces/ISheep.sol
+++ src/interfaces/ISheep.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 interface ISheep {
     function transferOwnership(address newOwner) external;
-    function eatSheep(address _victim, uint256 _amount, address _owner,uint256 _mintPercent) external;
+    function eatSheep(address _victim, uint256 _amount, address _owner, uint256 _mintPercent) external;
     function transferFrom(address from, address to, uint256 amount) external;
     function transfer(address to, uint256 amount) external returns (bool);
     function burnSheep(uint256 balSheepHere) external;
